dfa9613e43b949f2b5150568a91c54abcdb0f12e
Omar Kraidie <omppula_1@hotmail.com>
2021-03-25 11:04:42 +0200

    Refactored code

--- random_walk.py
+++ random_walk.py
@@ -18,13 +18,8 @@ class RandomWalk():
         # Keep taking steps untill the walk reaches desired lenght.
         while len(self.x_values) < self.num_points:
             # Decision which direction to go and how far to go in that direction
-            x_direction = choice([-1, 1])
-            x_distance = choice([1, 2, 3, 4, 5])
-            x_step = x_direction * x_distance
-
-            y_direction = choice([-1, 1])
-            y_distance = choice([1, 2, 3, 4, 5])
-            y_step = y_direction * y_distance
+            x_step = self.get_step()
+            y_step = self.get_step()
 
             # Reject moves that go nowhere
             if x_step == 0 and y_step == 0:
@@ -35,3 +30,12 @@ class RandomWalk():
 
             self.x_values.append(next_x)
             self.y_values.append(next_y)
+
+    def get_step(self):
+        """ Returns step value based on random choices. """
+
+        direction = choice([-1, 1])
+        distance = choice([1, 2, 3, 4, 5])
+        step = direction * distance
+
+        return step
--- readme.md
+++ readme.md
@@ -3,3 +3,5 @@
 -   [x] 15-1. Cubes
 -   [x] 15-2. Colored Cubes
 -   [x] 15-3. Molecular Motion
+-   [x] 15-4. Modified Random Walks (not included)
+-   [x] 15-5. Refarctoring
