b492775afe7446b1a56d1f64d527b28f7b3517d0
Omar Kraidie <omppula_1@hotmail.com>
2021-03-28 15:47:08 +0300

    Plotted more data

--- downloading_data/high_lows.py
+++ downloading_data/high_lows.py
@@ -16,7 +16,7 @@ def str_to_date(string):
 
 
 # Get dates and high and low tempratures from file
-data_file = './downloading_data/data/sitka_weather_2014.csv'
+data_file = './data/death_valley_2014.csv'
 
 with open(data_file) as f:
     reader = csv.reader(f)
@@ -24,27 +24,37 @@ with open(data_file) as f:
 
     dates, lows, highs = [], [], []
     for row in reader:
-        date = (row[0])
-        high = int(row[1])
-        low = int(row[2])
-        date_formatted = str_to_date(date)
-        high_c = fahrenheit_to_celsius(high)
-        low_c = fahrenheit_to_celsius(low)
-        dates.append(date_formatted)
-        highs.append(high_c)
-        lows.append(low_c)
+        try:
+            date = (row[0])
+            high = int(row[1])
+            low = int(row[3])
+
+        except ValueError:
+            print(f'{date} missing data')
+
+        else:
+            date_formatted = str_to_date(date)
+            high_c = fahrenheit_to_celsius(high)
+            low_c = fahrenheit_to_celsius(low)
+            dates.append(date_formatted)
+            highs.append(high_c)
+            lows.append(low_c)
 
 
 # Plot data with matplotlib
 fig = plt.figure(dpi=110, figsize=(10, 6))
-plt.plot(dates, highs, c='red')
+plt.plot(dates, highs, c='red', alpha=0.5)
+plt.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
-plt.title('Daily High Tempratures In Sitka - 2014', fontsize=20)
-plt.xlabel('', fontsize=14)
-plt.ylabel('Temperature (C)', fontsize=14)
-plt.tick_params(axis='both', which='major', labelsize=14)
+title = 'Daily high and low temperatures - 2014\nDeath Valley, CA'
+plt.title(title, fontsize=18)
+plt.xlabel('', fontsize=13)
+plt.ylabel('Temperature (C)', fontsize=13)
+plt.tick_params(axis='both', which='major', labelsize=13)
 
-plt.margins(x=0.0)
+plt.margins(x=0, y=0.1)
 fig.autofmt_xdate()
 
+plt.savefig('./data/images/high_low_death_valley.png')
 plt.show()
