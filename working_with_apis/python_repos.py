de88e81a3c9b7918dad342a431f4d3a98595040c
Omar Kraidie <omppula_1@hotmail.com>
2021-04-01 18:58:37 +0300

    Plotted API data with pygal

--- working_with_apis/python_repos.py
+++ working_with_apis/python_repos.py
@@ -1,4 +1,6 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
 # Make an API call and store the response
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
@@ -11,19 +13,28 @@ print(f'Total repositories: {response_dict["total_count"]}')
 
 # Explore information about the repositories
 repo_dicts = response_dict['items']
-print(f'Repositories returned: {len(repo_dicts)}')
 
-# Examine the first repository
-repo_dict = repo_dicts[0]
-
-# Loop Through all repositories returned by API request
-print('\nSelected information about each repository:')
-
+names, stars = [], []
 for repo_dict in repo_dicts:
-    print('\nName:', repo_dict['name'])
-    print('Owner:', repo_dict['owner']['login'])
-    print('Stars:', repo_dict['stargazers_count'])
-    print('Repository:', repo_dict['html_url'])
-    print('Created:', repo_dict['created_at'])
-    print('Updated:', repo_dict['updated_at'])
-    print('Desctiption:', repo_dict['description'])
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+# Visualize with pygal bar chart
+my_style = LS('#336699', base_style=LCS)
+my_style.title_font_size = 24
+my_style.label_font_size = 14
+my_style.major_label_font_size = 18
+
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+
+chart = pygal.Bar(my_config, style=my_style)
+chart.title = 'Most-Starred Python Projects on GitHub'
+chart.x_labels = names
+
+chart.add('', stars)
+chart.render_to_file('./working_with_apis/images/svg/python_repos.svg')
