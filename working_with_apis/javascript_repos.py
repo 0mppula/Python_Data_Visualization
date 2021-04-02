27cd0ae7ac9b5e5dc8e64561cb4296ecd9dcf409
Omar Kraidie <omppula_1@hotmail.com>
2021-04-02 14:15:29 +0300

    Plotted API data with pygal

--- working_with_apis/javascript_repos.py
+++ working_with_apis/javascript_repos.py
@@ -2,14 +2,14 @@ import requests
 import pygal
 from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
-# Make an API call and store the response
-url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+# Make an API request and store response
+url = 'https://api.github.com/search/repositories?q=language:javascript&sort=stars'
 r = requests.get(url)
-print('Status code:', r.status_code)
+print('API Request Status: ', r.status_code)
 
 # Store API response in a variable
 response_dict = r.json()
-print(f'Total repositories: {response_dict["total_count"]}')
+print('Total repositories: ', response_dict['total_count'])
 
 # Explore information about the repositories
 repo_dicts = response_dict['items']
@@ -20,14 +20,14 @@ for repo_dict in repo_dicts:
 
     plot_dict = {
         'value': repo_dict['stargazers_count'],
-        'label': repo_dict['description'] or "",
+        'label': repo_dict['description'],
         'xlink': repo_dict['html_url'],
     }
 
     plot_dicts.append(plot_dict)
 
 # Visualize with pygal bar chart
-my_style = LS('#336699', base_style=LCS)
+my_style = LS('#733380', base_style=LCS)
 my_style.title_font_size = 24
 my_style.label_font_size = 14
 my_style.major_label_font_size = 18
@@ -36,14 +36,14 @@ my_config = pygal.Config()
 my_config.value_formatter = lambda y: f'{y:,.0f}'  # format (123,456)
 my_config.x_label_rotation = 45
 my_config.show_legend = False
-my_config.tooltip_fancy_mode = False  # Hides title from tooltip on hover
+my_config.tooltip_fancy_mode = False
 my_config.truncate_label = 15
 my_config.show_y_guides = False
 my_config.width = 1000
 
 chart = pygal.Bar(my_config, style=my_style)
-chart.title = 'Most-Starred Python Projects on GitHub'
+chart.title = 'Most-Starred JavaScript Projects on GitHub'
 chart.x_labels = names
 
 chart.add('', plot_dicts)
-chart.render_to_file('./working_with_apis/images/svg/python_repos.svg')
+chart.render_to_file('./working_with_apis/images/svg/javascript_repos.svg')
