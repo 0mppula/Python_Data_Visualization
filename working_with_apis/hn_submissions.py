ed6fdd728b8f682829f4820d1f03b84f550df9c2
Omar Kraidie <omppula_1@hotmail.com>
2021-04-02 18:57:38 +0300

    Plotted hacker news data with pygal

--- working_with_apis/hn_submissions.py
+++ working_with_apis/hn_submissions.py
@@ -1,5 +1,7 @@
 import requests
 from operator import itemgetter
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
 # Meke an API call and store the respose
 api_url = 'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty'
@@ -8,8 +10,9 @@ print('Status code: ', r.status_code)
 
 # Process information about each submission
 submission_ids = r.json()
-submission_dicts = []
 
+titles, submission_dicts = [], []
+plot_counter = 0
 for submission_id in submission_ids[:30]:
     # Make a seperate API call to for each submission
     url = (
@@ -17,18 +20,41 @@ for submission_id in submission_ids[:30]:
     submission_r = requests.get(url)
     response_dict = submission_r.json()
 
+    title = response_dict['title']
+    titles.append(title)
+
     submission_dict = {
-        'title': response_dict['title'],
-        'link': f'http://news.ycombinator.com/item?id={str(submission_id)}',
-        'comments': response_dict.get('descendants', 0),  # return 0 in Null
+        'value': response_dict.get('descendants', 0),
+        'label': response_dict['title'],
+        'xlink': f'http://news.ycombinator.com/item?id={str(submission_id)}',
     }
 
     submission_dicts.append(submission_dict)
+    plot_counter += 1
+    print(f'Plotted {title} to chart... ({str(plot_counter)}/30)')
 
 submission_dicts = sorted(
-    submission_dicts, key=itemgetter('comments'), reverse=True)
+    submission_dicts, key=itemgetter('value'), reverse=True)
 
-for submission_dict in submission_dicts:
-    print('\nTitle:', submission_dict['title'])
-    print('Discussion link:', submission_dict['link'])
-    print('Comments:', submission_dict['comments'])
+# Visualize API data in a pygal bar chart
+my_style = LS('#733380')
+my_style.title_font_size = 24
+my_style.labe_font_size = 14
+my_style.major_label_font_size = 18
+
+my_config = pygal.Config()
+my_config.value_formatter = lambda y: f'{y:,.0f}'
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.tooltip_fancy_mode = False
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+
+chart = pygal.Bar(my_config, style=my_style)
+chart.title = 'Most Active Discussions Currently on Hacker News'
+chart.x_labels = titles
+
+chart.add('', submission_dicts)
+chart.render_to_file('./working_with_apis/images/svg/hn_submissions.svg')
+print('Chart is Ready!')
