6b28f214401733e3271bf05723d26acf880dbe40
Omar Kraidie <omppula_1@hotmail.com>
2021-04-02 19:51:11 +0300

    Refactored code

--- working_with_apis/python_repos.py
+++ working_with_apis/python_repos.py
@@ -2,48 +2,71 @@ import requests
 import pygal
 from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
-# Make an API call and store the response
-url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
-r = requests.get(url)
-print('Status code:', r.status_code)
 
-# Store API response in a variable
-response_dict = r.json()
-print(f'Total repositories: {response_dict["total_count"]}')
+def get_response():
+    """ Make an API call and return the response. """
+    url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+    r = requests.get(url)
+    print('Status code:', r.status_code)
+    return r
 
-# Explore information about the repositories
-repo_dicts = response_dict['items']
 
-names, plot_dicts = [], []
-for repo_dict in repo_dicts:
-    names.append(repo_dict['name'])
+def get_repo_dicts(response):
+    """ Return a set of dicts representing the most popular repositories. """
+    response_dict = r.json()
+    repo_dicts = response_dict['items']
+    print(f'Total repositories: {response_dict["total_count"]}')
+    return repo_dicts
 
-    plot_dict = {
-        'value': repo_dict['stargazers_count'],
-        'label': repo_dict['description'] or "",
-        'xlink': repo_dict['html_url'],
-    }
 
-    plot_dicts.append(plot_dict)
+def get_names_plot_dicts(repo_dicts):
+    """ Process the set of repository dicts, and pull out the data for plotting. """
+    names, plot_dicts = [], []
+    for repo_dict in repo_dicts:
+        name = repo_dict['name']
+        names.append(name)
 
-# Visualize with pygal bar chart
-my_style = LS('#336699', base_style=LCS)
-my_style.title_font_size = 24
-my_style.label_font_size = 14
-my_style.major_label_font_size = 18
+        # Some repositories have no description
+        # add 'no description' if description is ommitted
+        description = repo_dict['description']
+        if not description:
+            description = 'No description provided.'
 
-my_config = pygal.Config()
-my_config.value_formatter = lambda y: f'{y:,.0f}'  # format (123,456)
-my_config.x_label_rotation = 45
-my_config.show_legend = False
-my_config.tooltip_fancy_mode = False  # Hides title from tooltip on hover
-my_config.truncate_label = 15
-my_config.show_y_guides = False
-my_config.width = 1000
+        plot_dict = {
+            'value': repo_dict['stargazers_count'],
+            'label': description,
+            'xlink': repo_dict['html_url'],
+        }
 
-chart = pygal.Bar(my_config, style=my_style)
-chart.title = 'Most-Starred Python Projects on GitHub'
-chart.x_labels = names
+        plot_dicts.append(plot_dict)
+    return names, plot_dicts
 
-chart.add('', plot_dicts)
-chart.render_to_file('./working_with_apis/images/svg/python_repos.svg')
+
+def make_visualization(name, plot_dicts):
+    """ Visualize most popular repositories with pygal bar chart. """
+    my_style = LS('#336699', base_style=LCS)
+    my_style.title_font_size = 24
+    my_style.label_font_size = 14
+    my_style.major_label_font_size = 18
+
+    my_config = pygal.Config()
+    my_config.value_formatter = lambda y: f'{y:,.0f}'  # format (123,456)
+    my_config.x_label_rotation = 45
+    my_config.show_legend = False
+    my_config.tooltip_fancy_mode = False  # Hides title from tooltip on hover
+    my_config.truncate_label = 15
+    my_config.show_y_guides = False
+    my_config.width = 1000
+
+    chart = pygal.Bar(my_config, style=my_style)
+    chart.title = 'Most-Starred Python Projects on GitHub'
+    chart.x_labels = names
+
+    chart.add('', plot_dicts)
+    chart.render_to_file('./working_with_apis/images/svg/python_repos.svg')
+
+
+r = get_response()
+repo_dicts = get_repo_dicts(r)
+names, plot_dicts = get_names_plot_dicts(repo_dicts)
+make_visualization(names, plot_dicts)
